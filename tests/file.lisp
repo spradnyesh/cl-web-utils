e9091e406cbc7f8f4ebc4de532522756cc170fc0
Pradnyesh Sawant <spradnyesh@gmail.com>
2014-02-20 16:22:49 +0900

    write tests: file

--- tests/file.lisp
+++ tests/file.lisp
@@ -12,6 +12,18 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;; tests
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; normalize-file-name
 (test normalize-file-name
   (is (string-equal "abc.txt"
                     (web-utils::normalize-file-name "c:\\abc.txt"))))
+
+;; get-directory-path-string
+(test get-directory-path-string
+  (is (string-equal (get-directory-path-string #p"/home/pradyus/pers/../pers/vc/a.html")
+                    "/home/pradyus/pers/../pers/vc/")))
+
+;; get-new-path
+;; TODO
+#|(test get-new-path
+  (is (equal (web-utils::get-new-path #p"/home/pradyus/pers/vc/a.html")
+             #P"/home/pradyus/pers/vc/a1.html")))|#
